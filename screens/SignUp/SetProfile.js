f1841557d0718ad2c560ef250a5b49eb4ad5fedd
vivianlee03 <vivian@u.sogang.ac.kr>
2023-09-25 17:42:30 +0900

    Modify SetProfile Page (without photo components)

--- screens/SignUp/SetProfile.js
+++ screens/SignUp/SetProfile.js
@@ -1,8 +1,32 @@
 import React from "react";
 import { View } from "react-native";
+import {
+  AuthButtonContainer,
+  AuthContainer,
+  AuthTitle,
+} from "../../components/auth/AuthShared";
+import AuthButton from "../../components/auth/AuthButton";
+import AuthLayout from "../../components/auth/AuthLayout";
 
 const SetProfile = () => {
-  return <View></View>;
+  const username = "wandar";
+  return (
+    <AuthContainer>
+      <AuthLayout>
+        <AuthTitle>
+          Welcome {username}!{"\n"}Choose your profile
+        </AuthTitle>
+      </AuthLayout>
+      <AuthButtonContainer>
+        <AuthButton
+          text="Continue"
+          disabled={false}
+          // onPress={}
+          isYellow={false}
+        />
+      </AuthButtonContainer>
+    </AuthContainer>
+  );
 };
 
 export default SetProfile;
