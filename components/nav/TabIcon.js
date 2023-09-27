ec783bac179fbda7ad8862522469f02eb795920d
vivianlee03 <vivian@u.sogang.ac.kr>
2023-09-27 17:04:11 +0900

    Change Tab Nav Bar's Land Icon

--- components/nav/TabIcon.js
+++ components/nav/TabIcon.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
+import Location from "../../assets/location";
+import LocationOutline from "../../assets/location-outline";
 
-const TabIcon = ({ iconName, color, focused }) => {
-  return (
+const TabIcon = ({ iconName, color, focused, location }) => {
+  return !location ? (
     <Ionicons
       name={focused ? `${iconName}` : `${iconName}-outline`}
       color={color}
       size={24}
     />
+  ) : focused ? (
+    <Location />
+  ) : (
+    <LocationOutline />
   );
 };
 
