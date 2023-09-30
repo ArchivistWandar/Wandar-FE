24875a715f1ebe8846ae91327b46d5bb42b0f7fe
vivianlee03 <vivian@u.sogang.ac.kr>
2023-09-30 22:58:00 +0900

    4. Land - Friends/Browse Tab

--- screens/BrowseTab.js
+++ screens/BrowseTab.js
@@ -1,11 +1,38 @@
 import React from "react";
-import { Text } from "react-native";
 import { Container } from "../components/Shared";
+import UserList from "../components/friendsNav/UserList";
+
+const browseData = [
+  {
+    id: "3",
+    username: "minji",
+    lastUpdate: "Sep 21, 2023",
+    image: require("./../assets/images/profile10.png"),
+  },
+  {
+    id: "4",
+    username: "iamtaylor",
+    lastUpdate: "Aug 15, 2023",
+    image: require("./../assets/images/profile8.png"),
+  },
+  {
+    id: "5",
+    username: "hehe",
+    lastUpdate: "Aug 8, 2023",
+    image: require("./../assets/images/profile9.png"),
+  },
+  {
+    id: "6",
+    username: "ariana",
+    lastUpdate: "Aug 30, 2023",
+    image: require("./../assets/images/profile11.png"),
+  },
+];
 
 const BrowseTab = () => {
   return (
     <Container>
-      <Text style={{ color: "white" }}>Browse</Text>
+      <UserList data={browseData} />
     </Container>
   );
 };
