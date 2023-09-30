4614619b750cfceb997bd84066af87af7410b4b2
vivianlee03 <vivian@u.sogang.ac.kr>
2023-09-30 20:49:27 +0900

    9. MyPage - Edit Profile (🚨 bug: KeyboardAvoidingView is not working)

--- navigators/MyPageNav.js
+++ navigators/MyPageNav.js
@@ -1,82 +1,47 @@
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Ionicons } from "@expo/vector-icons";
+import MyPage from "../screens/MyPage/MyPage";
+import MyInfoEdit from "../screens/MyPage/MyInfoEdit";
+import MyProfileEdit from "../screens/MyPage/MyProfileEdit";
 import { colors } from "../colors";
-import TabIcon from "../components/nav/TabIcon";
-import MyTimeline from "../screens/MyPage/MyTimeline";
-import MyPhotos from "../screens/MyPage/MyPhotos";
 
-const Tab = createMaterialTopTabNavigator();
 const Stack = createStackNavigator();
 
 const MyPageNav = () => {
   return (
-    <Tab.Navigator
+    <Stack.Navigator
       screenOptions={{
-        tabBarStyle: {
+        headerMode: "screen",
+        headerBackTitleVisible: false,
+        headerTintColor: "white",
+        headerStyle: {
+          shadowColor: "rgba(255, 255, 255, 0.3)",
           backgroundColor: colors.backgroundColor,
-          paddingTop: "5%",
         },
-        tabBarActiveTintColor: "white",
-        tabBarIndicatorStyle: { backgroundColor: "white" },
+        headerTitleStyle: {
+          fontFamily: "JostSemiBold",
+          fontSize: 15,
+        },
       }}
     >
-      <Tab.Screen
-        name="Timeline"
+      <Stack.Screen
+        name="MyPage"
         options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <TabIcon color={color} focused={focused} iconName={"time"} />
-          ),
-          tabBarShowLabel: false,
+          headerTitle: "My page",
         }}
-      >
-        {() => (
-          <Stack.Navigator
-            screenOptions={{
-              headerTintColor: "white",
-              headerBackTitleVisible: false,
-              headerBackImage: ({ tintColor }) => (
-                <Ionicons color={tintColor} name="close" size={28} />
-              ),
-            }}
-          >
-            <Stack.Screen
-              name="MyTimeline"
-              component={MyTimeline}
-              options={{ headerShown: false }}
-            />
-          </Stack.Navigator>
-        )}
-      </Tab.Screen>
-      <Tab.Screen
-        name="Photos"
-        options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <TabIcon color={color} focused={focused} iconName={"apps"} />
-          ),
-          tabBarShowLabel: false,
-        }}
-      >
-        {() => (
-          <Stack.Navigator
-            screenOptions={{
-              headerTintColor: "white",
-              headerBackTitleVisible: false,
-              headerBackImage: ({ tintColor }) => (
-                <Ionicons color={tintColor} name="close" size={28} />
-              ),
-            }}
-          >
-            <Stack.Screen
-              name="MyPhotos"
-              component={MyPhotos}
-              options={{ headerShown: false }}
-            />
-          </Stack.Navigator>
-        )}
-      </Tab.Screen>
-    </Tab.Navigator>
+        component={MyPage}
+      />
+      <Stack.Screen
+        name="MyInfoEdit"
+        options={{ headerTransparent: true, headerTitle: "" }}
+        component={MyInfoEdit}
+      />
+      <Stack.Screen
+        name="MyProfileEdit"
+        options={{ headerTransparent: true, headerTitle: "" }}
+        component={MyProfileEdit}
+      />
+    </Stack.Navigator>
   );
 };
 
