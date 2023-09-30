3e764fb2fd43c69f1d0984e2989f49b3b993d7ab
vivianlee03 <vivian@u.sogang.ac.kr>
2023-09-30 17:40:10 +0900

    9. MyPage - My Photos

--- screens/MyPage/MyPhotos.js
+++ screens/MyPage/MyPhotos.js
@@ -1,11 +1,35 @@
 import React from "react";
-import { Text } from "react-native";
+import { FlatList, Image, useWindowDimensions } from "react-native";
 import { Container } from "../../components/Shared";
 
+const images = [
+  require("../../assets/images/jeju.png"),
+  require("../../assets/images/busan.png"),
+  require("../../assets/images/exhibition.png"),
+  require("../../assets/images/flowers.png"),
+  require("../../assets/images/plane.png"),
+];
+
 const MyPhotos = () => {
+  const numColumns = 3;
+  const { width } = useWindowDimensions();
+
+  const renderItem = ({ item }) => (
+    <Image
+      source={item}
+      style={{ width: width / numColumns, height: width / numColumns }}
+      resizeMode="cover"
+    />
+  );
+
   return (
     <Container>
-      <Text style={{ color: "white" }}>MyPhotos</Text>
+      <FlatList
+        numColumns={numColumns}
+        data={images}
+        keyExtractor={(item, index) => index.toString()} // Use index as the key
+        renderItem={renderItem}
+      />
     </Container>
   );
 };
