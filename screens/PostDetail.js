cc70663f437b2783da665da06e881cca68a7e84d
vivianlee03 <vivian@u.sogang.ac.kr>
2023-10-01 00:29:30 +0900

    When navigating to Detail Page, remove Tab Nav

---
+++ screens/PostDetail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { Container } from "../components/Shared";
+
+const PostDetail = () => {
+  return (
+    <Container>
+      <Text>PostDetail</Text>
+    </Container>
+  );
+};
+
+export default PostDetail;
