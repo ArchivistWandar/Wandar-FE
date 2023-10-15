036f98073075b4b324624832ce0b77f4d2416c15
vivianlee03 <vivian@u.sogang.ac.kr>
2023-10-15 23:46:11 +0900

    6. Upload Post - Edit ChooseLand Page & Add Preview Page

--- screens/UploadPost/ChooseLand.js
+++ screens/UploadPost/ChooseLand.js
@@ -1,8 +1,16 @@
-import React from "react";
-import { Image, Text, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  Dimensions,
+} from "react-native";
 import { Container } from "../../components/Shared";
-import Swiper from "react-native-swiper";
 import { Ionicons } from "@expo/vector-icons";
+import { colors } from "../../colors";
+import styled from "styled-components/native";
 
 const lands = [
   {
@@ -17,7 +25,62 @@ const lands = [
   },
 ];
 
-export default function ChooseLand() {
+const HeaderRightText = styled.Text`
+  color: ${colors.yellow};
+  font-size: 16px;
+  font-weight: 600;
+  margin-right: 10px;
+  font-family: "JostSemiBold";
+`;
+
+const windowWidth = Dimensions.get("window").width;
+
+export default function ChooseLand({ route, navigation }) {
+  const { selectedPhotos, memoText } = route.params;
+  const [selectedLandIndex, setSelectedLandIndex] = useState(0);
+
+  const HeaderRight = () => (
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate("Preview", {
+          selectedPhotos,
+          memoText: memoText,
+          selectedLand: lands[selectedLandIndex], // Adjust for 0-based array
+        })
+      }
+    >
+      <HeaderRightText>Next</HeaderRightText>
+    </TouchableOpacity>
+  );
+
+  useEffect(() => {
+    navigation.setOptions({ headerRight: HeaderRight });
+  }, [selectedLandIndex]);
+
+  console.log(selectedLandIndex);
+
+  const handleScroll = (event) => {
+    const newSelectedIndex = Math.round(
+      event.nativeEvent.contentOffset.x / windowWidth
+    );
+    setSelectedLandIndex(newSelectedIndex);
+  };
+
+  const scrollViewRef = useRef(); // Create a ref
+
+  const handleScrollLeft = () => {
+    scrollViewRef.current.scrollTo({
+      x: windowWidth * (selectedLandIndex - 1),
+      animated: true,
+    });
+  };
+
+  const handleScrollRight = () => {
+    scrollViewRef.current.scrollTo({
+      x: windowWidth * (selectedLandIndex + 1),
+      animated: true,
+    });
+  };
   return (
     <Container>
       <View
@@ -37,43 +100,64 @@ export default function ChooseLand() {
         >
           Choose a land for your upload
         </Text>
-        <Swiper
-          showsButtons={true}
-          buttonWrapperStyle={{ color: "#fff", backgroundColor: "transparent" }}
-          nextButton={
-            <Ionicons name="chevron-forward" size={30} color="#fff" />
-          }
-          prevButton={<Ionicons name="chevron-back" size={30} color="#fff" />}
-          dotColor="grey"
-          activeDotColor="white"
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+          }}
         >
-          {lands.map((land) => (
-            <View
-              key={land.id}
-              style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Image
-                source={land.image}
-                style={{ width: "70%", height: "70%" }}
-                resizeMode="contain"
-              />
-              <Text
+          <TouchableOpacity onPress={handleScrollLeft}>
+            <Ionicons name="chevron-back" size={30} color="#fff" />
+          </TouchableOpacity>
+          <ScrollView
+            ref={scrollViewRef}
+            horizontal
+            pagingEnabled
+            onScroll={handleScroll}
+            scrollEventThrottle={16}
+            showsHorizontalScrollIndicator={false}
+          >
+            {lands.map((land) => (
+              <View
                 style={{
-                  fontSize: 20,
-                  marginBottom: 40,
-                  fontFamily: "JostSemiBold",
-                  color: "white",
+                  width: windowWidth * 0.8,
+                  justifyContent: "center",
+                  alignItems: "center",
                 }}
+                key={land.id}
               >
-                {land.name}
-              </Text>
-            </View>
+                <Image
+                  source={land.image}
+                  style={{ width: "70%", height: "70%" }}
+                  resizeMode="contain"
+                />
+                <Text
+                  style={{
+                    fontSize: 20,
+                    marginBottom: 40,
+                    fontFamily: "JostSemiBold",
+                    color: "white",
+                  }}
+                >
+                  {land.name}
+                </Text>
+              </View>
+            ))}
+          </ScrollView>
+          <TouchableOpacity onPress={handleScrollRight}>
+            <Ionicons name="chevron-forward" size={30} color="#fff" />
+          </TouchableOpacity>
+        </View>
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          {lands.map((land, index) => (
+            <Text
+              key={index}
+              style={{ margin: 3, color: "white", fontSize: 15 }}
+            >
+              {selectedLandIndex === index ? "●" : "○"}
+            </Text>
           ))}
-        </Swiper>
+        </View>
       </View>
     </Container>
   );
--- screens/UploadPost/Preview.js
+++ screens/UploadPost/Preview.js
@@ -1,11 +1,59 @@
 import React from "react";
-import { Text } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { Container } from "../../components/Shared";
+import Collage from "../../components/Collage";
+import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function Preview() {
+const MemoContainer = styled.View`
+  margin: 25px;
+  margin-top: 0px;
+  margin-bottom: 50px;
+`;
+
+export default function Preview({ route, navigation }) {
+  const { selectedPhotos, memoText, selectedLand } = route.params;
+
+  const goToChooseLand = () => {
+    navigation.navigate("ChooseLand", {
+      selectedPhotos: route.params.selectedPhotos,
+      memoText: memoText,
+    });
+  };
+
   return (
     <Container>
-      <Text style={{ color: "white" }}>Preview</Text>
+      <ScrollView>
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            marginTop: 30,
+            justifyContent: "center",
+            flexDirection: "row",
+            gap: 10,
+          }}
+        >
+          <Text
+            style={{ fontFamily: "JostBold", fontSize: 20, color: "white" }}
+          >
+            {selectedLand.name}
+          </Text>
+        </View>
+        <Collage route={route} />
+        <MemoContainer>
+          <Text
+            style={{
+              fontFamily: "JostMedium",
+              fontSize: 14,
+              color: "white",
+              marginTop: 15,
+            }}
+          >
+            {memoText}
+          </Text>
+        </MemoContainer>
+      </ScrollView>
     </Container>
   );
 }
