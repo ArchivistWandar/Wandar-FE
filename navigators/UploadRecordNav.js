ae0b53c1955be8a380d3d9751606640bb2747687
vivianlee03 <vivian@u.sogang.ac.kr>
2023-11-05 01:04:16 +0900

    7. Upload Record - Modify image size for small screen

--- navigators/UploadRecordNav.js
+++ navigators/UploadRecordNav.js
@@ -1,16 +1,5 @@
 import React, { useState } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { colors } from "../colors";
-import {
-  TextInput,
-  TouchableOpacity,
-  Text,
-  StyleSheet,
-  Platform,
-  View,
-} from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import { BlurView } from "expo-blur";
 
 import SelectRPhotos from "../screens/UploadRecord/SelectRPhotos";
 import PreviewRecord from "../screens/UploadRecord/PreviewRecord";
@@ -25,21 +14,6 @@ const UploadRecordNav = () => {
         headerBackTitleVisible: false,
         headerTintColor: "white",
         headerTransparent: true, // 헤더의 배경을 투명하게 설정
-        // headerBackground: () =>
-        //   // 블러 처리된 배경을 설정
-        //   Platform.OS === "ios" ? (
-        //     <BlurView
-        //       tint="dark"
-        //       intensity={50}
-        //       style={StyleSheet.absoluteFill}
-        //     />
-        //   ) : (
-        //     <View style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }} />
-        //   ),
-        // headerTitleStyle: {
-        //   fontFamily: "JostSemiBold",
-        //   fontSize: 15,
-        // },
       }}
     >
       <Stack.Screen
@@ -51,13 +25,6 @@ const UploadRecordNav = () => {
       />
 
       <Stack.Screen name="PreviewRecord" component={PreviewRecord} />
-      {/* <Stack.Screen
-        name="DecoRecord"
-        options={{
-          headerTitle: "New record",
-        }}
-        component={DecoRecord}
-      /> */}
     </Stack.Navigator>
   );
 };
