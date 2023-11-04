b7835b2c2676a37385618bc52b0466e9a86541cb
vivianlee03 <vivian@u.sogang.ac.kr>
2023-11-05 00:30:37 +0900

    7. Upload Record - Header color & theme

--- navigators/UploadRecordNav.js
+++ navigators/UploadRecordNav.js
@@ -1,49 +1,19 @@
 import React, { useState } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { colors } from "../colors";
-import { TextInput, TouchableOpacity, Text, StyleSheet } from "react-native";
+import {
+  TextInput,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Platform,
+  View,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { BlurView } from "expo-blur";
 
 import SelectRPhotos from "../screens/UploadRecord/SelectRPhotos";
 import PreviewRecord from "../screens/UploadRecord/PreviewRecord";
-
-const EditableHeaderTitle = ({ initialTitle }) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [title, setTitle] = useState(initialTitle);
-
-  return isEditing ? (
-    <TextInput
-      value={title}
-      onChangeText={setTitle}
-      onEndEditing={() => {
-        if (title.trim() === "") {
-          setTitle(initialTitle);
-        }
-        setIsEditing(false);
-      }}
-      autoFocus
-      style={{ color: "white", fontFamily: "JostSemiBold", fontSize: 15 }}
-    />
-  ) : (
-    <TouchableOpacity
-      onPress={() => setIsEditing(true)}
-      style={{
-        flex: 1,
-        alignItems: "center",
-        flexDirection: "row",
-        gap: "10px",
-      }}
-    >
-      <Text
-        style={{ color: "white", fontFamily: "JostSemiBold", fontSize: 15 }}
-      >
-        {title}
-      </Text>
-      <Ionicons name="pencil" size={18} color={"white"} />
-    </TouchableOpacity>
-  );
-};
 
 const Stack = createStackNavigator();
 
@@ -54,23 +24,22 @@ const UploadRecordNav = () => {
         headerMode: "screen",
         headerBackTitleVisible: false,
         headerTintColor: "white",
-        headerStyle: {
-          shadowColor: "rgba(255, 255, 255, 0.2)",
-          backgroundColor: "transparent", // 배경색을 투명하게 설정
-        },
         headerTransparent: true, // 헤더의 배경을 투명하게 설정
-        headerBackground: () => (
-          // 블러 처리된 배경을 설정
-          <BlurView
-            tint="dark"
-            intensity={50}
-            style={StyleSheet.absoluteFill}
-          />
-        ),
-        headerTitleStyle: {
-          fontFamily: "JostSemiBold",
-          fontSize: 15,
-        },
+        // headerBackground: () =>
+        //   // 블러 처리된 배경을 설정
+        //   Platform.OS === "ios" ? (
+        //     <BlurView
+        //       tint="dark"
+        //       intensity={50}
+        //       style={StyleSheet.absoluteFill}
+        //     />
+        //   ) : (
+        //     <View style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }} />
+        //   ),
+        // headerTitleStyle: {
+        //   fontFamily: "JostSemiBold",
+        //   fontSize: 15,
+        // },
       }}
     >
       <Stack.Screen
@@ -81,13 +50,7 @@ const UploadRecordNav = () => {
         component={SelectRPhotos}
       />
 
-      <Stack.Screen
-        name="PreviewRecord"
-        options={{
-          headerTitle: () => <EditableHeaderTitle initialTitle="New record" />,
-        }}
-        component={PreviewRecord}
-      />
+      <Stack.Screen name="PreviewRecord" component={PreviewRecord} />
       {/* <Stack.Screen
         name="DecoRecord"
         options={{
