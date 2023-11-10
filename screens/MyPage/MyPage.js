2c9d553256946c8783becb9c8d933d96cdce3605
vivianlee03 <vivian@u.sogang.ac.kr>
2023-11-11 00:05:50 +0900

    Add Logout Button + Maintain LoggedIn

--- screens/MyPage/MyPage.js
+++ screens/MyPage/MyPage.js
@@ -2,8 +2,27 @@ import React from "react";
 import { Container } from "../../components/Shared";
 import MyPageTabNav from "../../navigators/MyPageTabNav";
 import MyInfo from "./MyInfo";
+import { logUserOut } from "../../apollo";
+import { useEffect } from "react";
+import { Text, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const MyPage = ({ navigation }) => {
+  const HeaderRight = () => (
+    <TouchableOpacity onPress={() => logUserOut()}>
+      <Ionicons
+        name="log-out-outline"
+        size={30}
+        color="#fff"
+        style={{ marginRight: "5%" }}
+      />
+    </TouchableOpacity>
+  );
+
+  useEffect(() => {
+    navigation.setOptions({ headerRight: HeaderRight });
+  });
+
   return (
     <Container>
       <MyInfo navigation={navigation} />
