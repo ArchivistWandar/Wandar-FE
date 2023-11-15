ab11a3a50166abf41818f49f22dd3eda5bd31243
우소정 <sojeong@usojeong-ui-MacBookPro.local>
2023-11-15 13:05:20 +0900

    without texture

--- components/TempLand.js
+++ components/TempLand.js
@@ -29,7 +29,7 @@ export default function TempLand() {
 
   useEffect(() => {
     // 모델 로드
-    Asset.loadAsync(require("../assets/glbAsset/square.glb"))
+    Asset.loadAsync(require("../assets/glbAsset2/bell3.glb"))
       .then(() => {
         setModelLoaded(true); // 로딩 완료 시 상태 업데이트
       })
@@ -131,14 +131,51 @@ export default function TempLand() {
       console.log(modelLoaded);
       // 모델 로드가 완료되면 씬에 추가
       const gltfLoader = new GLTFLoader();
+      const textureLoader = new THREE.TextureLoader();
+
+      const albedoTexture = textureLoader.load(
+        Asset.fromModule(require("../assets/glbTexture/universal.png")).uri,
+        // Add a callback for successful texture loading
+        () => {
+          console.log("Albedo texture loaded successfully");
+        },
+        // Add a callback for progress (optional)
+        undefined,
+        // Add a callback for errors in loading texture
+        (err) => {
+          console.error("Error loading albedo texture", err);
+        }
+      );
+
+      const emissiveTexture = textureLoader.load(
+        Asset.fromModule(require("../assets/glbTexture/emission.png")).uri
+      );
+
       gltfLoader.load(
-        Asset.fromModule(require("../assets/glbAsset/square.glb")).uri,
+        Asset.fromModule(require("../assets/glbAsset2/bell3.glb")).uri,
         (gltf) => {
-          gltf.scene.position.set(2, 5, 5);
-          scene.add(gltf.scene);
-          console.log("Model loaded and added to the scene");
-          console.log("Model position:", gltf.scene.position);
-          console.log("Model scale:", gltf.scene.scale);
+          const model = gltf.scene;
+          model.traverse((child) => {
+            if (child.isMesh) {
+              // Assign albedo texture
+              child.material.map = albedoTexture;
+
+              // Assign emissive texture
+              child.material.emissiveMap = emissiveTexture;
+              child.material.emissive = new THREE.Color(0xffffff); // This sets the color of the glow
+              child.material.emissiveIntensity = 1; // Adjust the intensity of the glow
+
+              // Ensure the material is updated on the GPU
+              child.material.needsUpdate = true;
+            }
+          });
+
+          model.position.set(0, 0, 3);
+          model.scale.set(10, 10, 10);
+          scene.add(model);
+          console.log("Model loaded and added to the scene with texture");
+          console.log("Model position:", model.position);
+          console.log("Model scale:", model.scale);
         },
         undefined, // 로드 진행 상황에 대한 콜백 함수 (필요시 사용)
         (error) => {
