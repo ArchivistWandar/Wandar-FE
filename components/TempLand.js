f3faec9bae92179f507b29ee7c172b8a8d2789ec
우소정 <sojeong@usojeong-ui-MacBookPro.local>
2023-11-15 14:05:34 +0900

    no camera

--- components/TempLand.js
+++ components/TempLand.js
@@ -1,18 +1,16 @@
 import React, { useEffect, useRef, useState } from "react";
 import { GLView } from "expo-gl";
-import { PanResponder, View } from "react-native";
 import { Renderer } from "expo-three";
 import {
   AmbientLight,
-  BoxGeometry,
+  PerspectiveCamera,
+  OrthographicCamera,
+  Scene,
+  PointLight,
+  SpotLight,
   Fog,
   GridHelper,
-  Mesh,
-  MeshStandardMaterial,
-  PerspectiveCamera,
-  PointLight,
-  Scene,
-  SpotLight,
+  TextureLoader,
 } from "three";
 import * as THREE from "three";
 import CameraControls from "camera-controls";
@@ -23,92 +21,74 @@ CameraControls.install({ THREE: THREE });
 
 export default function TempLand() {
   const [modelLoaded, setModelLoaded] = useState(false);
-  const timeoutRef = useRef();
+  const [textures, setTextures] = useState({});
   const cameraControlsRef = useRef(null);
-  const initialDistanceRef = useRef(null);
 
   useEffect(() => {
+    // 텍스처 로드
+    const loadTextures = async () => {
+      const textureLoader = new TextureLoader();
+      const albedo = await new Promise((resolve, reject) => {
+        textureLoader.load(
+          Asset.fromModule(require("../assets/glbTexture/universal.png")).uri,
+          resolve,
+          undefined,
+          reject
+        );
+      });
+
+      const emissive = await new Promise((resolve, reject) => {
+        textureLoader.load(
+          Asset.fromModule(require("../assets/glbTexture/emission.png")).uri,
+          resolve,
+          undefined,
+          reject
+        );
+      });
+
+      setTextures({ albedo, emissive });
+    };
+
     // 모델 로드
-    Asset.loadAsync(require("../assets/glbAsset2/bell3.glb"))
-      .then(() => {
-        setModelLoaded(true); // 로딩 완료 시 상태 업데이트
-      })
-      .catch((error) => console.error("Model loading error:", error));
+    const loadModel = async () => {
+      try {
+        await Asset.loadAsync(require("../assets/glbAsset2/bell3.glb"));
+        setModelLoaded(true);
+      } catch (error) {
+        console.error("Model loading error:", error);
+      }
+    };
 
-    // 컴포넌트 언마운트 시 타임아웃 클리어
-    return () => clearTimeout(timeoutRef.current);
+    loadTextures();
+    loadModel();
   }, []);
-
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderGrant: (evt, gestureState) => {
-        if (evt.nativeEvent.touches.length === 2) {
-          console.log("touches 2!!!");
-          const touch1 = evt.nativeEvent.touches[0];
-          const touch2 = evt.nativeEvent.touches[1];
-          const dx = touch1.pageX - touch2.pageX;
-          const dy = touch1.pageY - touch2.pageY;
-          initialDistanceRef.current = 1.5 * Math.sqrt(dx * dx + dy * dy);
-        }
-      },
-      onPanResponderMove: (evt, gestureState) => {
-        if (
-          evt.nativeEvent.touches.length === 2 &&
-          initialDistanceRef.current !== null
-        ) {
-          const touch1 = evt.nativeEvent.touches[0];
-          const touch2 = evt.nativeEvent.touches[1];
-          const dx = touch1.pageX - touch2.pageX;
-          const dy = touch1.pageY - touch2.pageY;
-          const distance = Math.sqrt(dx * dx + dy * dy);
-          const distanceDiff = distance - initialDistanceRef.current;
-          const scaleFactor = 0.005;
-
-          if (cameraControlsRef.current) {
-            if (distanceDiff > 0) {
-              cameraControlsRef.current.dolly(scaleFactor * distanceDiff, true);
-            } else {
-              cameraControlsRef.current.dolly(
-                -scaleFactor * Math.abs(distanceDiff),
-                true
-              );
-            }
-          }
-        } else if (
-          evt.nativeEvent.touches.length === 1 &&
-          cameraControlsRef.current
-        ) {
-          const panXScaleFactor = 0.001;
-          const panYScaleFactor = 0.001;
-          cameraControlsRef.current.truck(
-            -gestureState.dx * panXScaleFactor,
-            -gestureState.dy * panYScaleFactor,
-            true
-          );
-        }
-      },
-      onPanResponderRelease: () => {
-        initialDistanceRef.current = null;
-      },
-    })
-  ).current;
 
   const onContextCreate = async (gl) => {
     const { drawingBufferWidth: width, drawingBufferHeight: height } = gl;
+    const aspect = width / height;
     const sceneColor = 0x6ad6f0;
 
-    // Create a WebGLRenderer without a DOM element
+    const frustumSize = 10;
+    const orthoCamera = new OrthographicCamera(
+      (frustumSize * aspect) / -2,
+      (frustumSize * aspect) / 2,
+      frustumSize / 2,
+      frustumSize / -2,
+      1,
+      1000
+    );
+    orthoCamera.position.set(2, 5, 5);
+
     const renderer = new Renderer({ gl });
     renderer.setSize(width, height);
     renderer.setClearColor(sceneColor);
 
-    const camera = new PerspectiveCamera(70, width / height, 0.01, 1000);
-    camera.position.set(2, 5, 5);
-
     const scene = new Scene();
     scene.fog = new Fog(sceneColor, 1, 10000);
     scene.add(new GridHelper(10, 10));
+
+    const camera = new PerspectiveCamera(70, width / height, 0.01, 1000);
+    camera.position.set(2, 5, 5);
 
     const ambientLight = new AmbientLight(0x101010);
     scene.add(ambientLight);
@@ -122,50 +102,18 @@ export default function TempLand() {
     spotLight.lookAt(scene.position);
     scene.add(spotLight);
 
-    const cube = new TileMesh();
-    cube.position.set(0, 0, 0);
-    scene.add(cube);
-
-    if (modelLoaded) {
-      console.log("loading----------------------------------");
-      console.log(modelLoaded);
-      // 모델 로드가 완료되면 씬에 추가
+    if (modelLoaded && textures.albedo && textures.emissive) {
       const gltfLoader = new GLTFLoader();
-      const textureLoader = new THREE.TextureLoader();
-
-      const albedoTexture = textureLoader.load(
-        Asset.fromModule(require("../assets/glbTexture/universal.png")).uri,
-        // Add a callback for successful texture loading
-        () => {
-          console.log("Albedo texture loaded successfully");
-        },
-        // Add a callback for progress (optional)
-        undefined,
-        // Add a callback for errors in loading texture
-        (err) => {
-          console.error("Error loading albedo texture", err);
-        }
-      );
-
-      const emissiveTexture = textureLoader.load(
-        Asset.fromModule(require("../assets/glbTexture/emission.png")).uri
-      );
-
       gltfLoader.load(
         Asset.fromModule(require("../assets/glbAsset2/bell3.glb")).uri,
         (gltf) => {
           const model = gltf.scene;
           model.traverse((child) => {
             if (child.isMesh) {
-              // Assign albedo texture
-              child.material.map = albedoTexture;
-
-              // Assign emissive texture
-              child.material.emissiveMap = emissiveTexture;
-              child.material.emissive = new THREE.Color(0xffffff); // This sets the color of the glow
-              child.material.emissiveIntensity = 1; // Adjust the intensity of the glow
-
-              // Ensure the material is updated on the GPU
+              child.material.map = textures.albedo;
+              child.material.emissiveMap = textures.emissive;
+              child.material.emissive = new THREE.Color(0xffffff);
+              child.material.emissiveIntensity = 1;
               child.material.needsUpdate = true;
             }
           });
@@ -173,60 +121,27 @@ export default function TempLand() {
           model.position.set(0, 0, 3);
           model.scale.set(10, 10, 10);
           scene.add(model);
-          console.log("Model loaded and added to the scene with texture");
-          console.log("Model position:", model.position);
-          console.log("Model scale:", model.scale);
         },
-        undefined, // 로드 진행 상황에 대한 콜백 함수 (필요시 사용)
+        undefined,
         (error) => {
           console.error("An error happened during loading a model", error);
         }
       );
     }
 
-    // Create CameraControls and attach it to the renderer's canvas
     const cameraControls = new CameraControls(camera, gl.canvas);
     cameraControlsRef.current = cameraControls;
 
     let clock = new THREE.Clock();
-
-    function update() {
-      let delta = clock.getDelta();
-      cube.rotation.y += 0.05;
-      cube.rotation.x += 0.025;
-      cameraControls.update(delta);
-    }
-
-    const myCube = new TileMesh(10, 10);
-    myCube.position.set(0, 0, 0);
-    scene.add(myCube);
-
-    // Setup an animation loop
     const render = () => {
-      timeoutRef.current = requestAnimationFrame(render);
-      update();
+      requestAnimationFrame(render);
+      const delta = clock.getDelta();
+      cameraControls.update(delta);
       renderer.render(scene, camera);
       gl.endFrameEXP();
     };
     render();
   };
 
-  return (
-    <GLView
-      {...panResponder.panHandlers}
-      style={{ flex: 1 }}
-      onContextCreate={onContextCreate}
-    />
-  );
-}
-
-class TileMesh extends Mesh {
-  constructor(x, y) {
-    super(
-      new BoxGeometry(x, 0.1, y), // Change BoxBufferGeometry to BoxGeometry
-      new MeshStandardMaterial({
-        color: "seagreen",
-      })
-    );
-  }
+  return <GLView style={{ flex: 1 }} onContextCreate={onContextCreate} />;
 }
