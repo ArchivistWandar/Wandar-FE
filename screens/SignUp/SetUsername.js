bc9360e61886fae8dbb9fe56cfb75969076d7100
vivianlee03 <vivian@u.sogang.ac.kr>
2023-11-16 15:28:53 +0900

    Handle error in SetUsername with useState & useLazyQuery

--- screens/SignUp/SetUsername.js
+++ screens/SignUp/SetUsername.js
@@ -2,10 +2,6 @@ import React, { useRef, useState, useEffect } from "react";
 import AuthLayout from "../../components/auth/AuthLayout";
 import AuthButton from "../../components/auth/AuthButton";
 import {
-  removeWhitespace,
-  validateUsername,
-} from "../../components/auth/Utils";
-import {
   AuthButtonContainer,
   AuthErrorMessage,
   AuthTextBox,
@@ -13,42 +9,74 @@ import {
   AuthTextInput,
   AuthTitle,
 } from "../../components/auth/AuthShared";
-import { View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
 import { Container } from "../../components/Shared";
+import { gql, useLazyQuery } from "@apollo/client";
+
+const SEARCH_USERS = gql`
+  query SearchUsers($keyword: String!) {
+    searchUsers(keyword: $keyword) {
+      username
+    }
+  }
+`;
 
 const SetUsername = ({ route, navigation }) => {
   const { email, password } = route.params;
 
   const [username, setUsername] = useState("");
-  const [errorMessage, setErrorMessage] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   const [disabled, setDisabled] = useState(true);
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
 
   const inputRef = useRef();
   const goToProfile = () =>
-    navigation.navigate("SetProfile", {
-      email: email,
-      password: password,
-      username: username.replace("@", ""),
-    });
+    navigation.navigate("SetProfile", { email, password, username });
+
+  const [checkUsername, { loading, data }] = useLazyQuery(SEARCH_USERS);
 
   const _handleUsernameChange = (username) => {
-    // Remove whitespace and ensure "@" is at the beginning
-    let changedUsername = removeWhitespace(username);
-    if (!changedUsername.startsWith("@")) {
-      changedUsername = "@" + changedUsername;
+    if (username.length > 0 && !/^[A-Za-z0-9_]+$/.test(username)) {
+      setErrorMessage("Please use only English characters, numbers, or '_'.");
+      setIsCheckingUsername(false);
+      return;
     }
 
-    setUsername(changedUsername);
-    setErrorMessage(
-      validateUsername(changedUsername)
-        ? ""
-        : "Your username is already taken. Please try another."
-    );
+    setUsername(username);
+
+    if (username.length >= 3) {
+      setIsCheckingUsername(true);
+      checkUsername({ variables: { keyword: username } });
+    } else if (username.length > 0) {
+      setErrorMessage("Username must be at least 3 characters long.");
+      setIsCheckingUsername(false);
+    } else {
+      setErrorMessage("");
+      setIsCheckingUsername(false);
+    }
   };
 
   useEffect(() => {
-    setDisabled(!(username && !errorMessage));
-  }, [username, errorMessage]);
+    if (!loading) {
+      setIsCheckingUsername(false);
+      if (data && data.searchUsers.length > 0) {
+        setErrorMessage("Your username is already taken. Please try another.");
+      } else {
+        setErrorMessage("");
+      }
+    }
+  }, [data, loading, username.length]);
+
+  useEffect(() => {
+    setDisabled(
+      !(
+        username &&
+        username.length >= 3 &&
+        !errorMessage &&
+        !isCheckingUsername
+      )
+    );
+  }, [username, errorMessage, isCheckingUsername]);
 
   return (
     <Container>
@@ -57,19 +85,51 @@ const SetUsername = ({ route, navigation }) => {
         <AuthTextBox disabled={errorMessage}>
           <View>
             <AuthTextInfo>Username</AuthTextInfo>
-            <AuthTextInput
-              placeholder="@wandar"
-              placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
-              returnKeyType="next"
-              autoCapitalize={"none"}
-              ref={inputRef}
-              onLayout={() => inputRef.current.focus()}
-              value={username}
-              onChangeText={_handleUsernameChange}
-            />
+            <View style={{ flexDirection: "row" }}>
+              <Text style={{ fontFamily: "JostMedium", color: "white" }}>
+                @
+              </Text>
+              <AuthTextInput
+                placeholder="wandar"
+                placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+                returnKeyType="next"
+                autoCapitalize="none"
+                ref={inputRef}
+                onLayout={() => inputRef.current?.focus()}
+                value={username}
+                onChangeText={_handleUsernameChange}
+                keyboardType="default"
+              />
+            </View>
           </View>
         </AuthTextBox>
-        <AuthErrorMessage>{errorMessage}</AuthErrorMessage>
+        {isCheckingUsername ? (
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: "2%",
+            }}
+          >
+            <ActivityIndicator
+              size="small"
+              color="#fff"
+              style={{ marginLeft: "3%", marginRight: "2%" }}
+            />
+            <Text
+              style={{
+                fontFamily: "JostRegular",
+                color: "grey",
+                fontSize: 12,
+              }}
+            >
+              Checking username availability...
+            </Text>
+          </View>
+        ) : (
+          <AuthErrorMessage>{errorMessage}</AuthErrorMessage>
+        )}
+
         <AuthButtonContainer>
           <AuthButton
             text="Continue"
