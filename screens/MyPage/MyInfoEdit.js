32cc0c57034e0821abb4a12c47c35da2e72fe436
vivianlee03 <vivian@u.sogang.ac.kr>
2023-11-18 20:11:32 +0900

    EditProfile (except username & email)

--- screens/MyPage/MyInfoEdit.js
+++ screens/MyPage/MyInfoEdit.js
@@ -1,37 +1,144 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
-import { Container } from "../../components/Shared";
-import MyProfileEdit from "./MyProfileEdit";
-import { AuthButtonContainer } from "../../components/auth/AuthShared";
 import AuthButton from "../../components/auth/AuthButton";
-import AuthLayout from "../../components/auth/AuthLayout";
-import { ScrollView, TouchableOpacity, View } from "react-native";
+import {
+  ActivityIndicator,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import ProfileEditContainer from "../../components/ProfileEditContainer";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { ReactNativeFile } from "apollo-upload-client";
+import { LoadingContainer } from "../../components/Shared";
+
+export const MY_PAGE = gql`
+  query SeeMypage {
+    seeMypage {
+      username
+      email
+      avatar
+    }
+  }
+`;
+
+const EDIT_PROFILE_MUTATION = gql`
+  mutation EditProfile(
+    $avatar: Upload
+    $username: String
+    $email: String
+    $password: String
+  ) {
+    editProfile(
+      avatar: $avatar
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+    }
+  }
+`;
 
 const MyInfoEdit = ({ navigation }) => {
-  const [showPassword, setShowPassword] = useState(false);
-  const [username, setUsername] = useState("vivianlee");
-  const [email, setEmail] = useState("wandar@hello.com");
-  const [password, setPassword] = useState("abcdefghijk");
+  const { data, loading, refetch } = useQuery(MY_PAGE, {
+    fetchPolicy: "network-only",
+  });
+  const [editProfileMutation, { loading: editLoading }] = useMutation(
+    EDIT_PROFILE_MUTATION
+  );
 
+  const [profileImage, setProfileImage] = useState(null);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
   const [disabled, setDisabled] = useState(false);
+
+  useEffect(() => {
+    if (data && data.seeMypage) {
+      const {
+        avatar,
+        username: fetchedUsername,
+        email: fetchedEmail,
+      } = data.seeMypage;
+      setProfileImage(
+        avatar ? { uri: avatar } : require("../../assets/images/profile8.png")
+      );
+      setUsername(fetchedUsername);
+      setEmail(fetchedEmail);
+    }
+  }, [data]);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  const handleSave = () => {
-    // Handle save logic here (e.g., send updated data to the server).
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      const selectedImageUri = result.assets[0].uri;
+      setProfileImage({ uri: selectedImageUri });
+    }
   };
+
+  const handleSave = async () => {
+    let photoFile = null;
+
+    if (profileImage && profileImage.uri) {
+      photoFile = new ReactNativeFile({
+        uri: profileImage.uri,
+        type: `image/${profileImage.uri.split(".").pop()}`,
+        name: `profile.${profileImage.uri.split(".").pop()}`,
+      });
+    }
+
+    try {
+      const response = await editProfileMutation({
+        variables: {
+          avatar: photoFile,
+          username: username,
+          email: email,
+          password: password,
+        },
+      });
+
+      if (response.data.editProfile.ok) {
+        // Handle successful profile edit
+        alert("Profile updated successfully");
+      } else {
+        // Handle unsuccessful profile edit
+        alert("Failed to update profile");
+      }
+    } catch (error) {
+      // Handle error
+      console.error("Error editing profile:", error);
+      alert(`An error occurred: ${error.message}`);
+    }
+  };
+
+  if (loading)
+    return (
+      <LoadingContainer>
+        <ActivityIndicator size="small" color="white" />
+      </LoadingContainer>
+    );
 
   return (
     <ProfileEditContainer>
       <ScrollView>
         <ProfileImageContainer>
           {/* Original profile image */}
-          <ProfileImage source={require("../../assets/images/profile1.png")} />
-          <EditButton onPress={() => navigation.navigate(MyProfileEdit)}>
+          <ProfileImage source={profileImage} />
+          <EditButton onPress={pickImage}>
             <Ionicons name="pencil" size={24} color="white" />
           </EditButton>
         </ProfileImageContainer>
@@ -43,7 +150,6 @@ const MyInfoEdit = ({ navigation }) => {
             placeholder="Enter your username"
             placeholderTextColor={"grey"}
             value={username}
-            onChangeText={(text) => setUsername(text)}
           />
         </TextInputContainer>
 
@@ -54,7 +160,6 @@ const MyInfoEdit = ({ navigation }) => {
             placeholder="Enter your email"
             placeholderTextColor={"grey"}
             value={email}
-            onChangeText={(text) => setEmail(text)}
             keyboardType="email-address"
             autoCapitalize={"none"}
           />
@@ -83,9 +188,10 @@ const MyInfoEdit = ({ navigation }) => {
         <ButtonContainer>
           <AuthButton
             text="Save"
-            disabled={disabled}
+            disabled={disabled || editLoading}
             isYellow={false}
             loading={false}
+            onPress={handleSave}
           />
         </ButtonContainer>
       </ScrollView>
